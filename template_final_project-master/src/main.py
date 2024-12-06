8868beb81ff0fb1faf1490591405d3eea00e25e9
lfowler4 <lfowler4@binghamton.edu>
2024-12-05 22:05:12 -0500

    main.py

---
+++ template_final_project-master/src/main.py
@@ -0,0 +1,16 @@
+import pygame
+from Updated_Controller import Controller
+
+def main():
+    pygame.init()
+   # Run the controller
+    if __name__ == "__main__":
+       game = Controller()
+       game.mainloop()
+main()
+    
+    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+
+# https://codefather.tech/blog/if-name-main-python/
+if __name__ == '__main__':
+    main()
