51855d712b52f192dbad92cf83225f4208e29396
lfowler4 <lfowler4@binghamton.edu>
2024-12-05 22:09:29 -0500

    Update main.py

--- template_final_project-master/src/main.py
+++ template_final_project-master/src/main.py
@@ -12,5 +12,3 @@ main()
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
-if __name__ == '__main__':
-    main()
