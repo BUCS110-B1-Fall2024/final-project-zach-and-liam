b920922e3a384a2d931fa10af987efec4f2eba68
lfowler4 <lfowler4@binghamton.edu>
2024-12-05 22:14:15 -0500

    main.py

---
+++ template_final_project-master/main.py
@@ -0,0 +1,12 @@
+import pygame
+from Updated_Controller import Controller
+
+def main():
+    pygame.init()
+   # Run the controller
+    if __name__ == "__main__":
+       game = Controller()
+       game.mainloop()
+main()
+
+#Also Added In SRC If This Doesn't Work
